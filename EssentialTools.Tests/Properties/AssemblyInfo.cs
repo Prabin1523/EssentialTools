6bdf5288b0c893af6c5908f3f16683f603a96ba0
Prabin Pun <prabin1punah@gmail.com>
2018-10-04 14:14:47 -0400

    Add project files.

---
+++ EssentialTools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTools.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTools.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ac2223c-5569-4386-8397-d6e15205a5ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentialTools/Models/IValueCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace EssentialTools.Models
+{
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+}
